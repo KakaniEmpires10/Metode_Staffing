f070cb2f4bf3ac4dccaa8c0e377702f904e7e577
KakaniEmpires10 <muhammadalimkakani@gmail.com>
2023-02-07 09:23:54 +0700

    sudah bisa dua nilai

--- routes/Gillie.js
+++ routes/Gillie.js
@@ -4,11 +4,6 @@ const year = new Date().getFullYear();
 
 let hasilGillie = [];
 
-// langsung
-var total = 5;
-var parsial = 3;
-var minimal = 1;
-
 // tidak langsung
 var tidakLangsung = 1;
 var penyuluhan = 0.25;
@@ -23,35 +18,81 @@ router
     });
   })
   .post((req, res) => {
+    let submitValue = req.body.submit;
+
     // variabel
     let pTotal = parseInt(req.body.pasienTotal);
     let pParsial = parseInt(req.body.pasienParsial);
     let pMinimal = parseInt(req.body.pasienMinimal);
     let totalPasien = pTotal + pParsial + pMinimal;
 
-    // cari Jam Keperwatan Per hari
-    let jamKepLangsung = (pTotal * total) + (pParsial * parsial) + (pMinimal * minimal);
-    let jamKepTidakLangsung = totalPasien * tidakLangsung;
-    let jamPenyuluhan = totalPasien * penyuluhan;
+    if (submitValue === "rendah") {
+      // langsung
+      let total = 5;
+      let parsial = 3;
+      let minimal = 1;
 
-    let jamKepHari =
-      Math.round((jamKepLangsung + jamKepTidakLangsung + jamPenyuluhan) / totalPasien);
+      // cari Jam Keperwatan Per hari
+      let jamKepLangsung =
+        pTotal * total + pParsial * parsial + pMinimal * minimal;
+      let jamKepTidakLangsung = totalPasien * tidakLangsung;
+      let jamPenyuluhan = totalPasien * penyuluhan;
 
-    // formula
-    let kepDibutuhkanPerTahun = jamKepHari * totalPasien * 365;
-    let kepDiberikanPerTahun = 289 * 8;
+      let jamKepHari = Math.round(
+        (jamKepLangsung + jamKepTidakLangsung + jamPenyuluhan) / totalPasien
+      );
 
-    let hasil = kepDibutuhkanPerTahun / kepDiberikanPerTahun;
-    let jumlahPerawatDibutuhkan = Math.round(hasil);
+      // formula
+      let kepDibutuhkanPerTahun = jamKepHari * totalPasien * 365;
+      let kepDiberikanPerTahun = 289 * 8;
 
-    let duaPuluhPersenDariHasil = Math.round((jumlahPerawatDibutuhkan * 20) / 100);
+      let hasil = kepDibutuhkanPerTahun / kepDiberikanPerTahun;
+      let jumlahPerawatDibutuhkan = Math.round(hasil);
 
-    let jumlahSeluruh = duaPuluhPersenDariHasil + jumlahPerawatDibutuhkan;
-    
-    hasilGillie = [];
-    hasilGillie.push(jumlahSeluruh);
+      let duaPuluhPersenDariHasil = Math.round(
+        (jumlahPerawatDibutuhkan * 20) / 100
+      );
 
-    res.redirect("/gillies");
+      let jumlahSeluruh = duaPuluhPersenDariHasil + jumlahPerawatDibutuhkan;
+
+      hasilGillie = [];
+      hasilGillie.push(jumlahSeluruh);
+
+      res.redirect("/gillies");
+      
+    } else if (submitValue === "tinggi") {
+      let total = 7;
+      let parsial = 5;
+      let minimal = 2;
+
+      // cari Jam Keperwatan Per hari
+      let jamKepLangsung =
+        pTotal * total + pParsial * parsial + pMinimal * minimal;
+      let jamKepTidakLangsung = totalPasien * tidakLangsung;
+      let jamPenyuluhan = totalPasien * penyuluhan;
+
+      let jamKepHari = Math.round(
+        (jamKepLangsung + jamKepTidakLangsung + jamPenyuluhan) / totalPasien
+      );
+
+      // formula
+      let kepDibutuhkanPerTahun = jamKepHari * totalPasien * 365;
+      let kepDiberikanPerTahun = 289 * 8;
+
+      let hasil = kepDibutuhkanPerTahun / kepDiberikanPerTahun;
+      let jumlahPerawatDibutuhkan = Math.round(hasil);
+
+      let duaPuluhPersenDariHasil = Math.round(
+        (jumlahPerawatDibutuhkan * 20) / 100
+      );
+
+      let jumlahSeluruh = duaPuluhPersenDariHasil + jumlahPerawatDibutuhkan;
+
+      hasilGillie = [];
+      hasilGillie.push(jumlahSeluruh);
+
+      res.redirect("/gillies");
+    }
   });
 
 module.exports = router;
